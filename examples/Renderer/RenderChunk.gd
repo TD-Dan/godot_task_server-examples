eb7adc989548eb7848ab8d2646def140e5bb4831
Daniel Herkert <daniel.herkert@gmail.com>
2022-01-09 14:06:58 +0200

    Added Threaded Renderer example

---
+++ examples/Renderer/RenderChunk.gd
@@ -0,0 +1,58 @@
+extends ColorRect
+
+var my_work_item : TaskServerWorkItem
+var my_color_data : Color = Color() setget _set_my_color
+
+var rendering = false
+
+func _set_my_color(val):
+	my_color_data=val
+	color=val # Set color of this ColorRect
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$TaskServerClient.connect("work_progress",self,"_on_work_progress")
+	$TaskServerClient.connect("work_ready",self,"_on_work_ready")
+	_set_my_color(Color(0.1, 0.1, 0.1, 1))
+
+# Updates the RenderChunk
+func render(priority):
+	rendering = true
+	_set_my_color(Color(0, 0.5, 0, 1))
+	
+	my_work_item = TaskServerWorkItem.new()
+	my_work_item.data = Color(1,0,0,1) # data to manipulate with function
+	my_work_item.function = funcref(self, "render_task")
+	my_work_item.priority = priority
+	$TaskServerClient.post_work(my_work_item)
+
+# called inside a TaskWorker thread
+func render_task(data):
+	randomize()
+	var random_number = randf() #get a random number between 0 and 1
+	
+	#Simulate some render delay
+	OS.delay_msec(100 + random_number*200)	# Wait for 0.5 - 2 seconds
+	
+	#Calculate data
+	var color_value = 0.5+random_number*0.5
+	data = Color(color_value, color_value, color_value, 1)
+	
+	return data
+
+func _on_work_progress(work_item, progress):
+	if work_item == my_work_item:
+		_set_my_color(Color(0.4, 1, 0, 1))
+	
+func _on_work_ready(ready_work):
+	# work_item should be same as my_work_item
+	if ready_work == my_work_item:
+		print("Chunk render done in %s seconds" % my_work_item.metadata.time_s_execute)
+		# set our data to the "rendered" data
+		_set_my_color(ready_work.data)
+		rendering = false
+
+
+func _on_RenderChunk_mouse_entered():
+	if !rendering:
+		render(10)
---
+++ examples/Renderer/Renderer.gd
@@ -0,0 +1,67 @@
+extends Control
+
+var x_count = 20
+var y_count = 12
+
+enum R_STATE {
+	NONE,
+	INIT,
+	RENDER_INIT,
+	RENDERING,
+	PAUSE,
+	READY
+}
+
+var render_state = R_STATE.NONE setget _set_render_state
+var render_chunk_scene = preload("res://examples/Renderer/RenderChunk.tscn")
+onready var render_container = $HSplitContainer/RenderChunkContainer
+
+func _set_render_state(val):
+	render_state = val
+	if render_state < R_STATE.INIT:
+		# clear old render chunks
+		for n in render_container.get_children():
+			n.queue_free()
+		
+		# Create RenderChunks
+		render_container.columns = x_count
+		
+		for x in range(0,x_count*y_count):
+			render_container.add_child(render_chunk_scene.instance())
+		
+		render_state = R_STATE.INIT
+	
+	if render_state == R_STATE.RENDER_INIT:
+		for chunk in render_container.get_children():
+			chunk.render(100)
+		render_state = R_STATE.RENDERING
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_set_render_state(R_STATE.NONE)
+	
+
+
+func _on_XSpinBox_value_changed(value):
+	x_count = value
+	_set_render_state(R_STATE.NONE)
+
+
+func _on_YSpinBox2_value_changed(value):
+	y_count = value
+	_set_render_state(R_STATE.NONE)
+
+func _on_StartButton_pressed():
+	if render_state != R_STATE.INIT:
+		_set_render_state(R_STATE.NONE)
+	if render_state == R_STATE.INIT:
+		_set_render_state(R_STATE.RENDER_INIT)
+
+
+func _on_BackButton_pressed():
+	get_tree().change_scene("res://main.tscn")
+
+
+func _on_ClearButton_pressed():
+	_set_render_state(R_STATE.NONE)
